07262b4f9f37768cea55ad75b9326441bb1d7b4f
SiemensHalske <hendrksiemens1@gmail.com>
2023-11-13 09:07:35 +0100

    .

--- Java/src/b_interpret.py
+++ Java/src/b_interpret.py
@@ -1,32 +1,16 @@
 import sys
 
-# Brainfuck code for "Hello World!" without using print statement in Python
-bf_code = """
-++++++++[>++++++++<-]>[<++++>-]
-+<[>-<
-    [>++++<-]>[<++++++++>-]<[>++++++++<-]
-    +>[>
-
-        ++++++++++[>+++++<-]>+.-.[-]<
-    <[-]<->] <[>>
-
-        +++++++[>+++++++<-]>.+++++.[-]<
-<<-]] >[>
-
-    ++++++++[>+++++++<-]>.[-]<
-<-]<
-+++++++++++[>+++>+++++++++>+++++++++>+<<<<-]>-.>-.+++++++.+++++++++++.<.
->>.++.+++++++..<-.>>-
-[[-]<]
-"""
-
-# Brainfuck interpreter in Python
 def brainfuck_interpreter(code):
     """
-    Brainfuck interpreter that executes Brainfuck code and outputs result without using print.
+    Updated Brainfuck interpreter that executes Brainfuck code and returns the output as a string.
     """
+    # Filtering the code to include only Brainfuck commands
     code = [c for c in code if c in ['>', '<', '+', '-', '.', ',', '[', ']']]
+
+    # Building the bracket map for loop handling
     bracemap = build_bracemap(code)
+
+    # Initializing the cells (memory tape)
     cells = [0] * 30000
     code_ptr = 0
     cell_ptr = 0
@@ -36,46 +20,65 @@ def brainfuck_interpreter(code):
         command = code[code_ptr]
 
         if command == '>':
-            cell_ptr += 1
+            cell_ptr = (cell_ptr + 1) % 30000  # Circular tape to avoid out-of-range errors
+
         elif command == '<':
-            cell_ptr -= 1
+            cell_ptr = (cell_ptr - 1) % 30000  # Circular tape
 
         elif command == '+':
-            cells[cell_ptr] = (cells[cell_ptr] + 1) % 256
+            cells[cell_ptr] = (cells[cell_ptr] + 1) % 256  # 8-bit cells
 
         elif command == '-':
-            cells[cell_ptr] = (cells[cell_ptr] - 1) % 256
+            cells[cell_ptr] = (cells[cell_ptr] - 1) % 256  # 8-bit cells
 
         elif command == '.':
-            output.append(chr(cells[cell_ptr]))
+            output.append(chr(cells[cell_ptr]))  # Append the ASCII character to the output
 
         elif command == ',':
-            pass  # No input functionality for this interpreter
+            # Input is not implemented in this interpreter
+            pass
 
-        elif command == '[':
-            if cells[cell_ptr] == 0:
-                code_ptr = bracemap[code_ptr]
+        elif command == '[' and cells[cell_ptr] == 0:
+            code_ptr = bracemap[code_ptr]  # Jump forward to the matching ']'
 
-        elif command == ']':
-            if cells[cell_ptr] != 0:
-                code_ptr = bracemap[code_ptr]
+        elif command == ']' and cells[cell_ptr] != 0:
+            code_ptr = bracemap[code_ptr]  # Jump back to the matching '['
 
         code_ptr += 1
 
-    # Instead of print, we can use sys.stdout.write to output without newline
-    sys.stdout.write(''.join(output))   
-    sys.stdout.flush()
+    return ''.join(output)
 
 def build_bracemap(code):
     temp_bracestack, bracemap = [], {}
     for position, command in enumerate(code):
         if command == '[':
             temp_bracestack.append(position)
-        if command == ']':
+        elif command == ']':
             start = temp_bracestack.pop()
             bracemap[start] = position
             bracemap[position] = start
     return bracemap
 
-# Execute the Brainfuck interpreter with the "Hello World!" Brainfuck code
-brainfuck_interpreter(bf_code)
+# Example usage
+bf_code = """
+++++++++++
+ [
+  >+++++++>++++++++++>+++>+<<<<-
+ ]                       Schleife zur Vorbereitung der Textausgabe
+ >++.                    Ausgabe von 'H'
+ >+.                     Ausgabe von 'e'
+ +++++++.                'l'
+ .                       'l'
+ +++.                    'o'
+ >++.                    Leerzeichen
+ <<+++++++++++++++.      'W'
+ >.                      'o'
+ +++.                    'r'
+ ------.                 'l'
+ --------.               'd'
+ >+.                     '!'
+ >.                      Zeilenvorschub
+ +++.                    Wagenrücklauf
+"""
+output = brainfuck_interpreter(bf_code)
+print(output)  # Should print "Hello"
